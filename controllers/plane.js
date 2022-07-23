d8e204fa0059f4ada10572adb820689b567d35be
Sergey <97333505+konoplitskii@users.noreply.github.com>
2022-07-23 22:58:33 +0300

    Validation request create plane

--- controllers/plane.js
+++ controllers/plane.js
@@ -10,6 +10,33 @@ const getPlanes = async (req,res) => {
 }
 
 const createPlane = async (req,res) => {
+    const errors = {};
+
+    if(!req.body.name) {
+        errors.name = {message:"Пожалуйста укажите название"}
+    }
+    if(!req.body.price) {
+        errors.price = {message:"Пожалуйста укажите цену"}
+    }
+    if(!req.body.description) {
+        errors.description = {message:"Пожалуйста укажите описание"}
+    }
+    if(req.body.description && req.body.description.length > 1300) {
+        errors.description = {message:"Слишком длинное описание"}
+    }
+    if(!req.body.capacity) {
+        errors.description = {message:"Пожалуйста укажите вместимость"}
+    }
+
+    if(!req.file) {
+        errors.planeImg = {message:"Пожалуйста загрузите фото"}
+    }
+
+
+    if (Object.keys(errors).length > 0) {
+        return res.status(400).json(errors)
+    }
+
     try{
         const {name,price,description,capacity} = req.body
         const plane = await Plane.create({
@@ -30,7 +57,7 @@ const getPlane = async (req,res) => {
         const plane = await Plane.findById(req.params.id);
         res.status(200).json(plane);
     }catch (e) {
-        res.status(500).json({message:"Не удалось получить запрашиваемый самолёт, повторите попытку"})
+        res.status(400).json({message:"Не удалось получить запрашиваемый самолёт, повторите попытку"})
     }
 }
 
