37bb3321fc0f86d223487420ffcef18be7ab1ea8
Sergey <97333505+konoplitskii@users.noreply.github.com>
2022-07-24 09:48:29 +0300

    add store

---
+++ frontend/src/store/planes/planesSlice.js
@@ -0,0 +1,14 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+
+const planesSlice = createSlice({
+    name:'planes',
+    initialState:{
+        planes:null,
+        isError:false,
+        isLoading:false,
+        message:''
+    }
+});
+
+export default planesSlice.reducer;
